472c0339e2d1f8d51842cf0087bca1711a86d6da
Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-05-05 18:16:46 +0500

    Install stimulus to reset form after creating

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+# Reference: https://www.honeybadger.io/blog/chat-app-rails-actioncable-turbo/
 Bundler.require(*Rails.groups)
 
 module Chitchat
