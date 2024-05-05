3c14a3f9862ac992e5b251f5a296065688c66fc9
Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-05-05 18:05:20 +0500

    Create private chat feature

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,12 @@ class Message < ApplicationRecord
 
   after_create_commit { broadcast_append_to self.room }
   
+
+  def confirm_participant
+    if self.room.is_private
+      is_participant = Participant.where(user_id: self.user.id, room_id: self.room.id).first
+      throw :abort unless is_participant
+    end
+  end
+
 end
